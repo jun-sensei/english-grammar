0b426c59a83f19e5350e343fa19f8a8ebb34a4ec
片桐　惇介 <jun.k@skland.co.jp>
2026-04-21 19:54:09 +0900

    Cleanup project files and keep index.html only

--- grammar-quiz/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/english-grammar/', // ← ここを追加（スラッシュで囲むのを忘れずに！）
-})
